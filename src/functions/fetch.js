139a1004c5a32e6bb64abf8dda87648e552d9492
AvivMoshe11 <avivmoshe123@gmail.com>
2022-07-18 15:47:32 +0300

    change error to var

--- src/functions/fetch.js
+++ src/functions/fetch.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 module.exports = {
   name: "fetch",
   run: async (endpoint, tag) => {
-    let error;
+    var error;
     const response = await fetch(`https://api.clashroyale.com/v1/${endpoint}/${tag}`, {
       method: "GET",
       headers: { "Content-Type": "application/json", Authorization: `Bearer ${process.env.API_TOKEN}` },
