4916e12a3d8147b291c4b8c4afd133264d29a1c1
AvivMoshe11 <avivmoshe123@gmail.com>
2022-07-29 23:52:31 +0300

    proxy

--- src/functions/fetch.js
+++ src/functions/fetch.js
@@ -5,7 +5,8 @@ module.exports = {
   name: "fetch",
   run: async (endpoint, tag, extra = "") => {
     var error;
-    const response = await fetch(`https://api.clashroyale.com/v1/${endpoint}/${tag}/${extra}`, {
+    const response = await fetch(`https://proxy.royaleapi.dev/v1/${endpoint}/${tag}/${extra}`, {
+      //api.clashroyale.com
       method: "GET",
       headers: { "Content-Type": "application/json", Authorization: `Bearer ${process.env.API_TOKEN}` },
     }).catch((err) => (error = err));
